6cc7bca23850e7e4ff540aecb12f0443c834b791
senluchen2015 <senluchen2015@u.northwestern.edu>
2015-01-27 23:46:39 -0600

    extra credit

--- minet-netclass-w15/src/apps/http_server3.cc
+++ minet-netclass-w15/src/apps/http_server3.cc
@@ -34,6 +34,10 @@ struct connection_list_s
   connection *first,*last;
 };
 
+connection *get_connection(int, connection_list*);
+int make_listen_socket(int);
+int get_client_socket( int);
+
 void add_connection(int,connection_list *);
 void insert_connection(int,connection_list *);
 void init_connection(connection *con);
@@ -71,38 +75,177 @@ int main(int argc,char *argv[])
   }
 
   /* initialize and make socket */
-
   /* set server address*/
-
   /* bind listening socket */
-
   /* start listening */
+  sock = make_listen_socket(server_port);
+			
+  connections.first = NULL;	
+  connections.last = NULL;
+  
+	insert_connection(sock, &connections);	
+
+  maxfd = sock;
 
   /* connection handling loop */
   while(1)
   {
     /* create read and write lists */
-
+    
+    FD_ZERO(&readlist);
+    FD_ZERO(&writelist);
+    
+    FD_SET(sock, &readlist);
+    i = connections.first;
+		printf("looping through i states\n");
+		fflush(stdout);
+    while(i){
+			if(i->state == NEW){
+				init_connection(i);
+				FD_SET(i->sock, &readlist);
+				i->state = READING_HEADERS;
+			}
+			else if(i->state == READING_HEADERS){
+				FD_SET(i->sock, &readlist);
+			}
+			else if(i->state == READING_FILE){
+				FD_SET(i->fd, &readlist);
+  		}
+			else if(i->state == WRITING_RESPONSE || i->state == WRITING_FILE){
+				FD_SET(i->sock, &writelist);
+			}
+			i = i->next;
+    }
+		printf("before select\n");
+		fflush(stdout);
     /* do a select */
-
+		select(maxfd + 1, &readlist, &writelist, NULL, NULL);
+		
     /* process sockets that are ready */
-  }
+  	i = connections.first;
+		while(i){  
+	  	if (FD_ISSET(i->sock, &readlist)) {
+        /* for the accept socket, add accepted connection to connections */
+     	 	if (i->sock == sock)
+       	{
+          if ((sock2 = get_client_socket(sock)) > 0) {
+            printf("received connection request \n");
+            if (sock2 > maxfd) {
+              maxfd = sock2;
+            }
+            insert_connection(sock2, &connections);
+          }
+        }
+        else /* for a connection socket, handle the connection */
+        {
+          printf("received write request \n");
+          read_headers(i); 
+        }
+     	}
+			else if(FD_ISSET(i->fd, &readlist)){
+				read_file(i);
+			}
+			else if(FD_ISSET(i->sock, &writelist)){
+				/* handle writelist */
+				if(i->state == WRITING_RESPONSE){
+					write_response(i);
+				}
+				else{	
+					write_file(i);
+				}
+			}
+			i = i->next;
+		}  
+	}
+}
+
+connection *get_connection(int sock, connection_list *con_list){
+	connection *i = con_list -> first;
+	while(i){
+		if(i->sock == sock){
+			return i;
+		}
+		i = i->next;
+	}
+	return NULL;
 }
 
 void read_headers(connection *con)
 {
-  /* first read loop -- get request and headers*/
-
-  /* parse request to get file name */
-  /* Assumption: this is a GET request and filename contains no spaces*/
+ 	char temp_buf[BUFSIZE + 1];
+	struct stat filestat;
+	int flag;
+	int rc;
+	 /* first read loop -- get request and headers*/
+  while( (rc = recv(con->sock, &temp_buf, BUFSIZE, 0)) > 0) {
+    temp_buf[rc] = '\0';
+    
+		if (con->headers_read == 0) {
+      strcpy(con->buf, temp_buf);
+			con->headers_read = rc;
+    } 
+		else {
+      strcat(con->buf, temp_buf);
+			con->headers_read += rc;
+    }
+    printf(con->buf);
+    fflush(stdout);
+    if (strstr(con->buf, "\r\n\r\n")) {
+      printf("detected end of request \n");
+      break;
+    }
+  }
+	if(rc<0){
+		if(errno == EAGAIN){
+			printf("error EAGAIN \n");	
+			return;
+		}
+	}
+	/* parse request to get file name */
+	char *req = strtok(con->buf, " ");
   
+	/* Assumption: this is a GET request and filename contains no spaces*/
+	req = strtok(NULL, " ");
+	if(!req){
+		con->ok = false;
+	}
+	else{
+		con->ok = true;
+		/* Assumption: this is a GET request and filename contains no spaces*/
+		printf("request: %s \n", req);
+		fflush(stdout);
+		/* direct requests relative to current directory of server */
+		getcwd(con->filename, FILENAMESIZE);
+		//printf("current directory: %s \n", filename);
+		fflush(stdout);
+		strcat(con->filename, "/");
+		strcat(con->filename, req);
+		printf("relative path requested: %s \n", con->filename);
+		 /* try opening the file */
+		if (access(con->filename, F_OK) == -1) {
+			printf("error: no file exists \n");
+			con->ok = false;
+		} else {
+			printf("file found \n");
+			stat(con->filename, &filestat);
+		}
+	}
+   
+ 
   /* get file name and size, set to non-blocking */
-    
-    /* get name */
-    
-    /* try opening the file */
-      
+  if ((con->fd = open(con->filename, O_RDONLY)) < 0 || !(S_ISREG(filestat.st_mode))) {
+		printf("error reading requested file %s \n", con->filename);
+		fflush(stdout);
+		con->ok = false;
+	}   
+	else{
 	/* set to non-blocking, get size */
+		con->state = WRITING_RESPONSE;
+		flag = fcntl(con->fd, F_GETFL, 0);
+		fcntl(con->fd, F_SETFL, flag | O_NONBLOCK);
+		con->filelen = filestat.st_size;
+	}
+      
   
   write_response(con);
 }
@@ -125,10 +268,56 @@ void write_response(connection *con)
   if (con->ok)
   {
     /* send headers */
-  }
+    sprintf(ok_response, ok_response_f, con->filelen);
+    printf(ok_response);
+    fflush(stdout);
+		
+		if((rc = send(sock2, &ok_response[written], strlen(&ok_response[written]), 0)) > 0) {	
+			printf("sending with %d rc \n", rc);
+			fflush(stdout);
+			con->response_written += rc;
+		}
+		else if(rc<0){
+			if(errno == EAGAIN){
+				printf("error EAGAIN\n");
+				fflush(stdout);
+				return;
+			}
+			else{
+				con->ok = false;	
+			}
+		}
+		else{
+			printf("successfully send responses\n");
+			printf("con -> fd: %d\n",con->fd);
+			fflush(stdout);
+
+			con->state = READING_FILE;
+			read_file(con);
+		}
+	}
   else
   {
-  }  
+		if(rc = send(sock2, &notok_response[written], strlen(&notok_response[written]), 0));
+		{
+			con->response_written += rc;
+		}
+		if(rc<0){
+			if(errno == EAGAIN){
+				return;
+			}
+			else{
+				con->ok = false;	
+			}
+		}
+		else{
+			con->state = READING_FILE;
+			read_file(con);
+		}
+		close(con->sock);
+		con->state = CLOSED; 
+	} 
+	 
 }
 
 void read_file(connection *con)
@@ -189,7 +378,7 @@ int readnbytes(int fd,char *buf,int size)
 {
   int rc = 0;
   int totalread = 0;
-  while ((rc = minet_read(fd,buf+totalread,size-totalread)) > 0)
+  while ((rc = read(fd,buf+totalread,size-totalread)) > 0)
     totalread += rc;
 
   if (rc < 0)
@@ -204,13 +393,52 @@ int writenbytes(int fd,char *str,int size)
 {
   int rc = 0;
   int totalwritten =0;
-  while ((rc = minet_write(fd,str+totalwritten,size-totalwritten)) > 0)
+  while ((rc = write(fd,str+totalwritten,size-totalwritten)) > 0)
     totalwritten += rc;
   
   if (rc < 0)
     return -1;
   else
     return totalwritten;
+}
+
+/* source: recitation slides */
+int make_listen_socket( int port) {
+  struct sockaddr_in sin;
+  int sock;
+  sock = socket( AF_INET, SOCK_STREAM, 0);
+  if (sock < 0)
+    return -1;
+  memset(& sin, 0, sizeof( sin));
+  sin.sin_family = AF_INET;
+  sin.sin_addr.s_addr = htonl( INADDR_ANY);
+  sin.sin_port = htons( port);
+  if (bind( sock, (struct sockaddr *) &sin, sizeof( sin)) < 0) {
+    return -1;
+  }
+  // accept 5 connections on backlog
+  listen(sock, 5);
+  return sock;
+ }
+      
+int get_client_socket( int listen_socket) {
+  struct sockaddr_in sin;
+  int sock;
+  int flag;
+  socklen_t sin_len;
+  memset(& sin, 0, sizeof( sin));
+  sin_len = sizeof( sin);
+  if ((sock = accept( listen_socket, (struct sockaddr *) &sin, &sin_len)) < 0) {
+    return -1;
+  }
+
+	flag = fcntl(sock, F_GETFL, 0);
+  if(flag < 0){
+		flag = 0;
+	}
+
+ 	fcntl(sock, F_SETFL, flag | O_NONBLOCK);  
+  return sock;
 }
 
 
